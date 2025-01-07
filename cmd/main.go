c4f577e9e55b33caf20c71ee44fed1ba56dd4ad1
Ivan Gleym <gleymivan@icloud.com>
2025-01-07 16:33:13 +0300

    database config

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,29 @@ import (
 
 	todo "github.com/ggleym/rest-golang"
 	"github.com/ggleym/rest-golang/pkg/handler"
+	"github.com/ggleym/rest-golang/pkg/repository"
+	"github.com/ggleym/rest-golang/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handler := new(handler.Handler)
-	srv := new(todo.Server)
-
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
-		log.Fatalf("Error occurred while running the server: %s", err.Error())
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
 	}
 
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handler := handler.NewHandler(service)
+
+	srv := new(todo.Server)
+
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+		log.Fatalf("Error occurred while running the server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
